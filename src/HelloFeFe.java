ab0739fdc581e1ce6c37f288b4c74aad693fd9af
Tremaine Scott <Trezzy52@gmail.com>
2019-01-16 21:31:31 -0500

    Second commit. Good evening, Java Class 1

--- src/HelloFeFe.java
+++ src/HelloFeFe.java
@@ -1,5 +1,6 @@
 public class HelloFeFe {
     public static void main(String[]args){
         System.out.println("hello feifa");
+        System.out.println("1st player is Barci and 2nd plays is Juventus");
     }
 }
